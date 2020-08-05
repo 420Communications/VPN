198df48d716d23bf36bba70b828e4e53a263bbe5
Muhammad Moaz <66547583+DevilRuler1@users.noreply.github.com>
2020-08-05 20:31:53 +0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+sudo apt update
+sudo apt install strongswan strongswan-pki
+mkdir -p ~/pki/{cacerts,certs,private}
+chmod 700 ~/pki
+ipsec pki --gen --type rsa --size 4096 --outform pem > ~/pki/private/ca-key.pem
+ipsec pki --self --ca --lifetime 3650 --in ~/pki/private/ca-key.pem --type rsa --dn "CN=VPN root CA" --outform pem > ~/pki/cacerts/ca-cert.pem
+ipsec pki --gen --type rsa --size 4096 --outform pem > ~/pki/private/server-key.pem
+ipsec pki --pub --in ~/pki/private/server-key.pem --type rsa \
+    | ipsec pki --issue --lifetime 1825 \
+        --cacert ~/pki/cacerts/ca-cert.pem \
+        --cakey ~/pki/private/ca-key.pem \
+        --dn "CN=104.129.129.100" --san "104.129.129.100" \
+        --flag serverAuth --flag ikeIntermediate --outform pem \
+    >  ~/pki/certs/server-cert.pem
+sudo cp -r ~/pki/* /etc/ipsec.d/
+sudo ufw allow OpenSSH
+sudo ufw enable
+sudo ufw allow 500,4500/udp
+ip route | grep default
+  
